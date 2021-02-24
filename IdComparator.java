c3d7093b0295eb693b8045327506de7187a022bb
Brian Steele <brianrsteele@gmail.com>
2021-02-23 18:33:56 -0600

    new branch to work on scheduler class

--- IdComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class IdComparator implements Comparator<Process> {
-
-	@Override
-	public int compare(Process o1, Process o2) {
-		return o1.getId() - o2.getId();
-	}
-}
